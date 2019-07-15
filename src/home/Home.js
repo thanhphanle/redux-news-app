e341650c1b4ae9683939784113b244ec8d9f509e
ThanhPL <phanlethanh93@gmail.com>
2019-07-15 21:42:36 +0700

    Add basic redux

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import './Home.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../actions';
 
-class Home extends Component {
+function Home() {
+    const counter = useSelector(state => state.counter);
+    const isLogged = useSelector(state => state.isLogged);
+    const dispatch = useDispatch();
 
-    render() {
-        return(
-            <div className="Home">
-                <div>
-                    <h3>News App with Redux</h3>
-                </div>
-                <div>
-                    <a href="/news">Vnexpress</a>
-                </div>
+    return (
+        <div className="Home">
+            <div>
+                <h3>News App with Redux</h3>
             </div>
-        );
-    }
+            <div>
+                <a href="/news">Vnexpress</a>
+            </div>
+            <div>Redux</div>
+            <div>
+                <p>Counter {counter}</p>
+                <button onClick={() => dispatch(increment(5))}>+</button>
+                <button onClick={() => dispatch(decrement())}>-</button>
+                {
+                    isLogged ? <h3>Valuable information I shoudn't see</h3> : ''
+                }
+            </div>
+        </div>
+    );
 }
 
 export default Home;
